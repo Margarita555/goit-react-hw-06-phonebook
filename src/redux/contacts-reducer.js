6c38a05caf8cd10d78a0de4b09a0aee521379177
Margarita555 <a_rita@ukr.net>
2021-12-25 21:20:34 +0200

    +

--- src/redux/contacts-reducer.js
+++ src/redux/contacts-reducer.js
@@ -1,29 +1,46 @@
+import { createReducer } from '@reduxjs/toolkit';
+import * as actions from './contacts-actions';
 import { combineReducers } from 'redux';
 
-const items = (state = [], { type, payload }) => {
+const items = createReducer([], {
+  [actions.addContact]: (state, { payload }) => [payload, ...state],
+  [actions.deleteContact]: (state, { payload }) =>
+    state.filter(contact => contact.name !== payload),
   //   console.log(payload);
-  switch (type) {
-    case 'contacts/add':
-      return [payload, ...state];
-    case 'contacts/delete':
-      console.log(state.filter(contact => contact.name !== payload));
-      return state.filter(contact => contact.name !== payload);
+});
 
-    default:
-      return state;
-  }
-};
-
-const filter = (state = '', { type, payload }) => {
-  switch (type) {
-    case 'contacts/changeFilter':
-      return payload;
-    default:
-      return state;
-  }
-};
+const filter = createReducer('', {
+  [actions.changeFilter]: (_, { payload }) => payload,
+});
 
 export default combineReducers({
   items,
   filter,
 });
+// const items = (state = [], { type, payload }) => {
+//   //   console.log(payload);
+//   switch (type) {
+//     case 'contacts/add':
+//       return [payload, ...state];
+//     case 'contacts/delete':
+//       console.log(state.filter(contact => contact.name !== payload));
+//       return state.filter(contact => contact.name !== payload);
+
+//     default:
+//       return state;
+//   }
+// };
+
+// const filter = (state = '', { type, payload }) => {
+//   switch (type) {
+//     case 'contacts/changeFilter':
+//       return payload;
+//     default:
+//       return state;
+//   }
+// };
+
+// export default combineReducers({
+//   items,
+//   filter,
+// });
